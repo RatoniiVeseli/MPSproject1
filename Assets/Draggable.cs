d636255395bcabd70806a27d0c9fa643044c0d3f
ISTNgit <iustin.gaman@gmail.com>
2018-11-05 01:38:16 +0200

    You can chose your cards now

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -2,6 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 	
@@ -9,9 +10,10 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 	public Transform placeholderParent = null;
 
 	GameObject placeholder = null;
-	
-	public void OnBeginDrag(PointerEventData eventData) {
-		Debug.Log ("OnBeginDrag");
+
+
+    public void OnBeginDrag(PointerEventData eventData) {
+		//Debug.Log ("OnBeginDrag");
 		
 		placeholder = new GameObject();
 		placeholder.transform.SetParent( this.transform.parent );
@@ -26,7 +28,7 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 		parentToReturnTo = this.transform.parent;
 		placeholderParent = parentToReturnTo;
 		this.transform.SetParent( this.transform.parent.parent );
-		
+        NextRound.cardsChosen++;
 		GetComponent<CanvasGroup>().blocksRaycasts = false;
 	}
 	
@@ -57,14 +59,64 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 	}
 	
 	public void OnEndDrag(PointerEventData eventData) {
-		Debug.Log ("OnEndDrag");
+		//Debug.Log ("OnEndDrag");
 		this.transform.SetParent( parentToReturnTo );
 		this.transform.SetSiblingIndex( placeholder.transform.GetSiblingIndex() );
 		GetComponent<CanvasGroup>().blocksRaycasts = true;
 
 		Destroy(placeholder);
 	}
-	
-	
-	
+
+    [Header("Text Boxes")]
+    public Text abilitiesUI;
+    public Text powerUI;
+    public Text defenseUI;
+
+    [Header("Images on card")]
+    public Image player;
+    public Image frame;
+
+    [Header("Stats")]
+    public int power;
+    public int defense;
+    /* aici depinde de voi cum retineti abilitatile. */
+    public List<int> abilities = new List<int>();
+    public string[] abilitiesText = { "Flying", "Trample", "Deathtouch", "Defender", "Reach", "First Strike" };
+
+    public void updateCardUI(int round)
+    {
+        powerUI.text = power.ToString();
+        defenseUI.text = defense.ToString();
+        abilitiesUI.text = abilitiesText[abilities[0]];
+        if (round == 1)
+            player.sprite = Resources.Load<Sprite>("Footballers/" + Random.Range(40, 80).ToString());
+        if (round == 2)
+        {
+            player.sprite = Resources.Load<Sprite>("Func/" + Random.Range(1, 34).ToString());
+            frame.sprite = Resources.Load<Sprite>("Spell");
+            powerUI.text = "SPE";
+            defenseUI.text = "LL";
+        }
+        if (round == 3)
+        {
+            player.sprite = Resources.Load<Sprite>("Leader/" + Random.Range(1, 40).ToString());
+            frame.sprite = Resources.Load<Sprite>("Leader");
+            powerUI.text = "LEA";
+            defenseUI.text = "DER";
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
 }
---
+++ Assets/NextRound.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NextRound : MonoBehaviour {
+
+    public GameObject cardTemplate;
+    public Canvas canvas;
+    public Button genDeck; // sper sa mearga lol
+    public static int cardsChosen;
+
+    // Use this for initialization
+    void Start () {
+        cardsChosen = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cardsChosen == 11)
+        {
+            genDeck.interactable = true;
+
+            // clean the table
+            if (cardsChosen == 11)
+            {
+                foreach (Transform child in GameObject.Find("Quarterback").transform)
+                {
+                    child.parent = null;
+                }
+
+                foreach (Transform child in GameObject.Find("Middle").transform)
+                {
+                    child.parent = null;
+                }
+            }
+        }
+
+        if (cardsChosen == 17)
+        {
+            genDeck.interactable = true;
+
+            // clean the table
+            if (cardsChosen == 17)
+                foreach (Transform child in GameObject.Find("Forward").transform)
+                    child.parent = null;
+        }
+
+        if (cardsChosen == 19)
+        {
+            foreach (Transform child in GameObject.Find("Quarterback").transform)
+            {
+                child.parent = null;
+            }
+
+            foreach (Transform child in GameObject.Find("Middle").transform)
+            {
+                child.parent = null;
+            }
+
+            foreach (Transform child in GameObject.Find("Forward").transform)
+            {
+                child.parent = null;
+            }
+        }
+    }
+
+    public void nextHand()
+    {
+        genDeck.interactable = false;
+        Debug.Log("Negat" + cardsChosen);
+        for (int i = 0; i < 12 && cardsChosen < 11; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Quarterback").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            //newCard.GetComponent<Draggable>().player.sprite = Resources.Load<Sprite>("Footballers/" + Random.Range(40, 80).ToString());
+            //Debug.Log(System.IO.Path.GetFileName("Footballers/" + Random.Range(40, 80).ToString()));
+            newCard.GetComponent<Draggable>().updateCardUI(1);
+        }
+
+        for (int i = 0; i < 12 && cardsChosen < 11; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Middle").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            //newCard.GetComponent<Draggable>().player.sprite = Resources.Load<Sprite>("Footballers/" + Random.Range(40, 80).ToString());
+            newCard.GetComponent<Draggable>().updateCardUI(1);
+        }
+
+        if (cardsChosen == 11)
+            cardsChosen++;
+
+        for (int i = 0; i < 10 && cardsChosen == 12; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Forward").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            //newCard.GetComponent<Draggable>().player.sprite = Resources.Load<Sprite>("Footballers/" + Random.Range(40, 80).ToString());
+            newCard.GetComponent<Draggable>().updateCardUI(2);
+        }
+
+        if (cardsChosen == 17)
+            cardsChosen++;
+
+        for (int i = 0; i < 14 && cardsChosen == 18; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Forward").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            newCard.GetComponent<Draggable>().updateCardUI(3);
+        }
+        for (int i = 0; i < 13 && cardsChosen == 18; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Quarterback").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            newCard.GetComponent<Draggable>().updateCardUI(3);
+        }
+        for (int i = 0; i < 13 && cardsChosen == 18; i++)
+        {
+            GameObject newCard = Instantiate(cardTemplate);
+            newCard.transform.SetParent(GameObject.Find("Middle").transform);
+            newCard.GetComponent<Draggable>().power = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().defense = Random.Range(1, 10);
+            newCard.GetComponent<Draggable>().abilities.Add(Random.Range(0, 6));
+            newCard.GetComponent<Draggable>().updateCardUI(3);
+        }
+    }
+}
